3518c79133c24f93b870b52bc405d683a3e636d0
Drew Gulino <dgulino@republicwirless.com>
2022-06-03 20:59:26 +0000

    increased block size to speed transfers of <=1GB files

--- src/dog_file_transfer.erl
+++ src/dog_file_transfer.erl
@@ -3,7 +3,7 @@
 
 -include_lib("kernel/include/file.hrl").
 
--define(BLOCK_SIZE, 4096).
+-define(BLOCK_SIZE, 131072).
 
 %% ------------------------------------------------------------------
 %% API Function Exports
