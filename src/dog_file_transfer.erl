41ef9c990a05d8d62129e4003cc919c4f8879e6f
Drew Gulino <dgulino@republicwirless.com>
2022-06-09 17:57:09 +0000

    fixed debug log entry

--- src/dog_file_transfer.erl
+++ src/dog_file_transfer.erl
@@ -196,7 +196,7 @@ publish_file_fetch(Hostkey, Filename) ->
                             LocalFilePath = ?FILE_LOCATION_BASE  ++ dog_common:to_list(Hostkey) ++ "/fetch/" ++ dog_common:to_list(Filename),
                             filelib:ensure_dir(filename:dirname(LocalFilePath) ++ "/"),                                                     
                             ok = file:write_file( LocalFilePath, Response, [raw, write, binary]),                                               
-                            lager:info("Response Size: ~p",[length(Response)]), 
+                            lager:info("Response size in bytes: ~p",[erlang:size(Response)]), 
                             Response;
                         <<"text/json">> ->
                             case hd(jsx:decode(Response)) of
