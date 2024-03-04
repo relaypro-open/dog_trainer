8646792131cbb3488c700871dc329a22120eab75
Drew Gulino <1379399+dgulinobw@users.noreply.github.com>
2024-03-04 15:35:46 -0500

    no longer requiring ports in a service

--- test/api_test.sh
+++ test/api_test.sh
@@ -31,7 +31,7 @@ get ${BASEURL}/service?name=drewtest
 echo -e "{\n\"name\":\"drewtest2\"\n}" > ${TESTDIR}/service_update.json
 put "${TESTDIR}/service_update.json" "${BASEURL}/service/${SERVICE_ID}"
 putc "${TESTDIR}/service_update.json" "${BASEURL}/service/${SERVICE_ID}" 303
-putc "${TESTDIR}/service_update_no_ports.json" "${BASEURL}/service/${SERVICE_ID}" 400
+putc "${TESTDIR}/service_update_no_ports.json" "${BASEURL}/service/${SERVICE_ID}" 303
 putc "${TESTDIR}/service_update_string_ports.json" "${BASEURL}/service/${SERVICE_ID}" 400
 get ${BASEURL}/service/${SERVICE_ID}
 delete ${BASEURL}/service/${SERVICE_ID}
