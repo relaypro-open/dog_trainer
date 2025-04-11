f4ac97942769cdc61acd3065a17fa1a24d2f5e39
Drew Gulino <1379399+dgulinobw@users.noreply.github.com>
2025-04-11 11:55:55 -0400

    reenabled rethinkdb connection pool

--- src/dog_rethink.erl
+++ src/dog_rethink.erl
@@ -12,6 +12,6 @@
     | {error, {atom(), Reason :: binary()}}.
 run(Fun) ->
     {ok, RethinkTimeout} = application:get_env(dog_trainer, rethink_timeout_ms),
-    {ok, Conn} = gen_rethink_session:get_connection(dog_session),
-    gen_rethink:run(Conn, Fun, RethinkTimeout).
-    %dog_db_pool_sup:run(?RETHINKDB_POOL, Fun, RethinkTimeout).
+    dog_db_pool_sup:run(?RETHINKDB_POOL, Fun, RethinkTimeout).
+    %{ok, Conn} = gen_rethink_session:get_connection(dog_session),
+    %gen_rethink:run(Conn, Fun, RethinkTimeout).
